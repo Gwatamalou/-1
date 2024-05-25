94adf0dd9a18e5b169acb65d864d58b45dad3721
Gwatamalou <96253655+Gwatamalou@users.noreply.github.com>
2024-05-25 19:53:47 +0600

    Update homework5.py

--- homework5.py
+++ homework5.py
@@ -4,13 +4,14 @@ print(immutable_var)
 """
 пункт 3 задания изменитие неизменяемую колекцию...
 ВЫЗОВ ПРИНЯТ!
-
 """
 
 tlist = list(immutable_var)
 tlist.append('easy win')
 immutable_var = tuple(tlist)
 print(immutable_var)
+
+#      ╮ (. ❛ ᴗ ❛.) ╭
 
 mutable_list = ['Тапинамбур', 14, True, [0, 1, 'c', 4]]
 mutable_list[0], mutable_list[3][1] = 'UGU', False
